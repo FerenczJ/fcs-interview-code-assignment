1c4b722349d46bcef70ec6282aaae26b23c4f542
jozsef.ferencz <jozsef.ferencz@chess.com>
2026-08-14 14:52:34 +0200

    chore(location): enhanced error handling in location lookup.

--- java-assignment/src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
+++ java-assignment/src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
@@ -1,11 +1,17 @@
 package com.fulfilment.application.monolith.location;
 
 import com.fulfilment.application.monolith.warehouses.domain.ports.LocationResolver;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class LocationGateway implements LocationResolver {
 
   @Override
   public Location resolveByIdentifier(String identifier) {
-    return Location.valueOf(identifier);
+    try {
+      return Location.valueOf(identifier);
+    } catch (IllegalArgumentException | NullPointerException e) {
+      return null;
+    }
   }
 }
