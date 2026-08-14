b639a74372ee5d1d2a609876f2ab720c63ff1c46
jozsef.ferencz <jozsef.ferencz@chess.com>
2026-08-14 12:07:20 +0200

    feat(location): implement resolveByIdentifier; change Location type to enum; add tests. refs #1

--- java-assignment/src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
+++ java-assignment/src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
@@ -1,25 +1,11 @@
 package com.fulfilment.application.monolith.location;
 
-import com.fulfilment.application.monolith.warehouses.domain.models.Location;
 import com.fulfilment.application.monolith.warehouses.domain.ports.LocationResolver;
-import java.util.List;
 
 public class LocationGateway implements LocationResolver {
 
-  private final List<Location> locations = List.of(
-          new Location("ZWOLLE-001", 1, 40),
-          new Location("ZWOLLE-002", 2, 50),
-          new Location("AMSTERDAM-001", 5, 100),
-          new Location("AMSTERDAM-002", 3, 75),
-          new Location("TILBURG-001", 1, 40),
-          new Location("HELMOND-001", 1, 45),
-          new Location("EINDHOVEN-001", 2, 70),
-          new Location("VETSBY-001", 1, 90)
-  );
-
   @Override
   public Location resolveByIdentifier(String identifier) {
-    // TODO implement this method
-    throw new UnsupportedOperationException("Unimplemented method 'resolveByIdentifier'");
+    return Location.valueOf(identifier);
   }
 }
--- java-assignment/src/main/java/com/fulfilment/application/monolith/warehouses/domain/models/Location.java
+++
@@ -1,3 +0,0 @@
-package com.fulfilment.application.monolith.warehouses.domain.models;
-
-public record Location(String identification, int zone, int capacity) { }
