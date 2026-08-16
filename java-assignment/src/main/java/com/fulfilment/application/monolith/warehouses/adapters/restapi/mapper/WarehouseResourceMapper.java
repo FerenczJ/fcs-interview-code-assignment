416f0f8b14bbb9b9e044e781c167a88b2a1973a4
jozsef.ferencz <jozsef.ferencz@chess.com>
2026-08-16 17:30:38 +0200

    chore(): Fixed mapper warnings at application startup.

--- java-assignment/src/main/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/mapper/WarehouseResourceMapper.java
+++ java-assignment/src/main/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/mapper/WarehouseResourceMapper.java
@@ -1,9 +1,14 @@
 package com.fulfilment.application.monolith.warehouses.adapters.restapi.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface WarehouseResourceMapper {
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "archivedAt", ignore = true)
     com.fulfilment.application.monolith.warehouses.domain.models.Warehouse toDomain(com.warehouse.api.beans.Warehouse apiModel);
+
+    @Mapping(target = "id", ignore = true)
     com.warehouse.api.beans.Warehouse toApi(com.fulfilment.application.monolith.warehouses.domain.models.Warehouse domainModel);
 }
