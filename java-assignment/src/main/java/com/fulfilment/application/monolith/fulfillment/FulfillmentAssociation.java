ddbaa373330aff8a578588ff893ed6e1105c818b
jozsef.ferencz <jozsef.ferencz@chess.com>
2026-08-16 15:09:33 +0200

    chore(): fixed broken tests.

--- java-assignment/src/main/java/com/fulfilment/application/monolith/fulfillment/FulfillmentAssociation.java
+++ java-assignment/src/main/java/com/fulfilment/application/monolith/fulfillment/FulfillmentAssociation.java
@@ -6,8 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "fulfillment_association")
